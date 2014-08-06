5d5471029cb008f83395a26288bf90a7124874d0
zhesu <zhesu@wireless-plus-058-079.usc.edu>
2014-08-06 16:54:48 -0700

    restart xcode

--- PopupViewController.h
+++ PopupViewController.h
@@ -7,7 +7,14 @@
 //
 
 #import <UIKit/UIKit.h>
+#import <QuartzCore/QuartzCore.h>
+#import "AppDelegate.h"
 
 @interface PopupViewController : UIViewController
+
+@property (strong, nonatomic) IBOutlet UIView *popUpView;
+@property (weak, nonatomic) IBOutlet UIImageView *logoImg;
+@property (weak, nonatomic) IBOutlet UILabel *messageLabel;
+- (void)showInView:(UIView *)aView withImage:(UIImage *)image withMessage:(NSString *)message animated:(BOOL)animated;
 
 @end
