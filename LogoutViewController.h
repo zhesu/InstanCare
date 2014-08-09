38cf8a70fbf310104f26b4a0baa260c53c2aebe1
zhesu <zhesu@wireless-plus-061-016.usc.edu>
2014-08-09 15:27:09 -0700

    logout success

---
+++ LogoutViewController.h
@@ -0,0 +1,14 @@
+//
+//  LogoutViewController.h
+//  InstanCare
+//
+//  Created by Zhe Su on 8/9/14.
+//  Copyright (c) 2014 Zhe Su. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <Parse/Parse.h>
+@interface LogoutViewController : UIViewController
+- (IBAction)logoutButton:(id)sender;
+
+@end
